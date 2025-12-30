3ddef62344cd0a89c63d55b61d624f0104cb2427
AmanDwivwdicc <dwivediaman104@gmail.com>
2025-12-30 16:38:43 +0000

    conversion of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all js code as newer version 
+
+//alert("aman don") not allowed cause were working in nodejs , not browser
+
+let name="aman"
+let age=20.2
+let isLoggedin=true
+
+console.log(typeof age)
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,7 @@
+let score="33"
+
+console.log(typeof score);
+
+let valueInNumber=Number(score) // convert to number
+
+console.log(typeof valueInNumber);
