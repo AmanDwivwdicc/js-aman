003fef1f940f579aeb85c2bbd8ff9c6255092296
AmanDwivwdicc <dwivediaman104@gmail.com>
2025-12-30 13:54:19 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+console.log("aman")
+const accountId= 144553
+let accountEmail="dwivd@gmail.com"
+var accpass="2342"
+accountCity="jaypur"
+let accountState;
+
+// accountId=2 // not allowed
+accountEmail="hc@hc.com"
+accpass="3344"
+accountCity="delhi"
+/*
+prefer not to use var 
+because of issue of block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accpass,accountCity, accountState])
