14fc6aa9e74b0d594e3a67d666f5086bf6893d07
AmanDwivwdicc <dwivediaman104@gmail.com>
2026-01-01 07:06:43 +0000

    datatypes summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,29 @@
 let score="33"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber=Number(score) // convert to number
 
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
+
+//********************operations*********************** */
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+//array ,objects,functions
+
+const heros=["shaktiman","naagraj","doga"]
+let myObj={
+    name: "aman",
+    age: 22,
+    hobbies: ["reading","coding","playing"]
+}
+
+const myFunc=function(){
+    console.log("hello world");
+}
+
+
